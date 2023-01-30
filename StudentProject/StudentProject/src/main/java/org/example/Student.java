35b27a273631db0af55e83efab47d617b4d39290
jimdeal <jimdeal@hotmail.com>
2023-01-30 15:11:45 +0000

    add in code for enum increment & decrement
    (thank god for stackoverflow:
    https://stackoverflow.com/questions/17006239/whats-the-best-way-to-implement-next-and-previous-on-an-enum-type
    )
    
    Next step convert the current main method to unit tests WIP

--- StudentProject/StudentProject/src/main/java/org/example/Student.java
+++ StudentProject/StudentProject/src/main/java/org/example/Student.java
@@ -2,8 +2,21 @@ package org.example;
 
 public class Student {
     public enum Grade {
-        A,B,C,D,E,F
+        A,B,C,D,E,F;
+
+
+        private static final Grade[] vals = values();
+
+        public Grade getNext() {
+            return vals[(this.ordinal() + 1) % vals.length];
+        }
+        public Grade getPrevious() {
+            return vals[(this.ordinal() - 1) % vals.length];
+        }
     }
+
+    private final int UPPER_GROUP = 5;
+    private final int LOWER_GROUP = 0;
 
     private String name;
     private Grade grade;
@@ -12,17 +25,49 @@ public class Student {
 
     public Student(String name, Grade grade, int group) {
         this.name = name;
-        this.grade = grade;
-        this.group = group;
+        if(!checkGrade(grade)){
+            throw new IllegalArgumentException("Invalid Grade");
+        } else {
+            this.grade = grade;
+
+        }
+        if(!checkGroup(group)){
+            throw new IllegalArgumentException("Invalid Group");
+        } else {
+            this.group = group;
+        }
     }
+
+    private boolean checkGrade(Grade grade){
+        boolean ret = false;
+        for (Grade gradIt : Grade.values()) {
+            if(grade==gradIt)
+            {
+                ret = true;
+            }
+        }
+        return ret;
+    }
+
+    private boolean checkGroup(int group){
+        return ((group>LOWER_GROUP)&&(group<UPPER_GROUP));
+    }
+
 
     public Grade getGrade(){
         return this.grade;
     }
     public void upGradeStudentGrade() {
+        if(this.grade != Grade.A){
+            Grade nextGrade =  grade.getPrevious();
+            this.grade = nextGrade;
+        }
     }
     public void downGradeStudentGrade(){
-
+        if(this.grade != Grade.F){
+            Grade previousGrade =  grade.getNext();
+            this.grade = previousGrade;
+        }
     }
     public String getName(){
         return this.name;
