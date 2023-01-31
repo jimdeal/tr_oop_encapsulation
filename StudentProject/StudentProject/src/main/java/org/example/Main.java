e08065dea4b5317c5f0c80ee4e99be570ce7ab8b
jimdeal <jimdeal@hotmail.com>
2023-01-31 15:17:54 +0000

    Add unit tests - may need to find out how to do testing for constructor throwing exception.

--- StudentProject/StudentProject/src/main/java/org/example/Main.java
+++ StudentProject/StudentProject/src/main/java/org/example/Main.java
@@ -3,36 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-
-        Student student1 = new Student("fred", Student.Grade.A, 3);
-        student1.upGradeStudentGrade();
-        Student.Grade gr1 = student1.getGrade();
-
-        Student student2 = new Student("fred", Student.Grade.B, 3);
-        student2.upGradeStudentGrade();
-        Student.Grade gr2 = student2.getGrade();
-
-        Student student3 = new Student("fred", Student.Grade.F, 3);
-        student3.upGradeStudentGrade();
-        Student.Grade gr3 = student3.getGrade();
-
-
-        Student student4 = new Student("fred", Student.Grade.A, 3);
-        student4.downGradeStudentGrade();
-        Student.Grade gr4 = student4.getGrade();
-
-        Student student5 = new Student("fred", Student.Grade.C, 3);
-        student5.downGradeStudentGrade();
-        Student.Grade gr5 = student5.getGrade();
-
-        Student student6 = new Student("fred", Student.Grade.F, 3);
-        student6.downGradeStudentGrade();
-        Student.Grade gr6 = student6.getGrade();
-
-
         // causes exception
         // Student student3 = new Student("fred", Student.Grade.A, 10);
-
-
     }
 }
--- StudentProject/StudentProject/src/test/java/org/example/StudentTest.java
+++ StudentProject/StudentProject/src/test/java/org/example/StudentTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,13 +16,46 @@ class StudentTest {
 
     @Test
     void upGradeStudentGrade() {
+        Student student1 = new Student("fred", Student.Grade.A, 3);
+        student1.upGradeStudentGrade();
+        Student.Grade gr1 = student1.getGrade();
+        assertEquals(gr1, Student.Grade.A);
+
+        Student student2 = new Student("fred", Student.Grade.B, 3);
+        student2.upGradeStudentGrade();
+        Student.Grade gr2 = student2.getGrade();
+        assertEquals(gr2, Student.Grade.A);
+
+        Student student3 = new Student("fred", Student.Grade.F, 3);
+        student3.upGradeStudentGrade();
+        Student.Grade gr3 = student3.getGrade();
+        assertEquals(gr3, Student.Grade.E);
     }
 
     @Test
     void downGradeStudentGrade() {
+        Student student4 = new Student("fred", Student.Grade.A, 3);
+        student4.downGradeStudentGrade();
+        Student.Grade gr4 = student4.getGrade();
+        assertEquals(gr4, Student.Grade.B);
+
+        Student student5 = new Student("fred", Student.Grade.C, 3);
+        student5.downGradeStudentGrade();
+        Student.Grade gr5 = student5.getGrade();
+        assertEquals(gr5, Student.Grade.D);
+
+        Student student6 = new Student("fred", Student.Grade.F, 3);
+        student6.downGradeStudentGrade();
+        Student.Grade gr6 = student6.getGrade();
+        assertEquals(gr6, Student.Grade.F);
+
     }
 
     @Test
     void getName() {
+        Student student7 = new Student("fred", Student.Grade.F, 3);
+        String name = student7.getName();
+        assertEquals(name, "fred");
     }
+
 }
